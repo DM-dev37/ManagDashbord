5c9243d9b83d885d9cbe1a355b86a28ee6e4f150
DM-dev37 <dilanebertrand1@gmail.com>
2026-03-10 21:43:43 +0100

    mise a jour de la sidebar

--- app/(components)/sidebar/page.tsx
+++ app/(components)/sidebar/page.tsx
@@ -1,5 +1,32 @@
+"use client";
+
+import { Menu } from "lucide-react";
 import React from "react";
 
 export default function SideBar() {
-  return <div></div>;
+  return (
+    <div className="px-4 border-gray-600 border h-screen">
+      {/* top logo */}
+      <div className="flex gap-4 justify-between md:justify-normal items-center pt-8">
+        <h1 className="text-sm font-semibold pt-2">
+          <span className="text-2xl font-black text-blue-500">R</span>EDSTOCK
+        </h1>
+
+        <button
+          className="md:hidden p-3 bg-gray-200 rounded-full hover:bg-blue-300"
+          onClick={() => {}}
+        >
+          <Menu className="size-4" />
+        </button>
+      </div>
+
+      {/* link */}
+      <div className="flex flex-1 mt-8"></div>
+
+      {/* footer */}
+      <div>
+        <p className="text-center text-gray-500 text-sx ">&copy; 2026 DM.dev</p>
+      </div>
+    </div>
+  );
 }
